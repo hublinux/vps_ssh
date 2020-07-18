b74b40787706b5749dc64f84b10a28e607a4928f
hublinux <42792864@qq.com>
2020-07-18 18:54:29 +0800

    Update onesshd.sh

--- onesshd.sh
+++ onesshd.sh
@@ -12,12 +12,16 @@ green "=========================================================="
  blue "首先，备份sshd配置文件，并将其文件名修改为sshd_config.back "
  blue "备份后的文件与原文件在同一个目录 "
 green "=========================================================="
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config.back
-echo $?
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.back
+sudo echo $?
+green "=========================================================="
+ blue "检查wget是否安装 "
+green "=========================================================="
+sudo apt install wget -y
 green "=========================================================="
  blue "准备下载sshd配置文件，并修改VPS中的sshd_config "
 green "=========================================================="
-wget -N --no-check-certificate "https://raw.githubusercontent.com/hublinux/vps_ssh/master/sshd_config" && cat sshd_config > /etc/ssh/sshd_config
+sudo wget -N --no-check-certificate "https://raw.githubusercontent.com/hublinux/vps_ssh/master/sshd_config" && cat sshd_config > /etc/ssh/sshd_config
 echo $?
 green "=========================================================="
 blue "重新启动sshd服务 "
